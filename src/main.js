dec968130f03fbcb785ad179d3b6b51aef13978a
11xiaomin <2421939406@qq.com>
2019-12-18 16:55:12 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUi from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+import './styles/index.less' // 引入初始化样式
 
 Vue.config.productionTip = false
+Vue.use(ElementUi)// 全局注册
 
 new Vue({
   router,
