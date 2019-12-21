446247c8c56cc4ff11a8b109e73d11729978e98f
11xiaomin <2421939406@qq.com>
2019-12-21 23:02:19 +0800

    面包屑组件封装

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,12 @@
 import layoutAside from './home/layout-aside'
 import layoutHeader from './home/layout-header'
+import BreadCrumb from './common/bread-crumb'
 // 所有自定义组件的插件
 export default {
   install (Vue) {
     // alert(1)
     Vue.component('layout-aside', layoutAside) // 注册 左侧导航组件
     Vue.component('layout-header', layoutHeader) // 注册头部组件
+    Vue.component('bread-crumb', BreadCrumb) // 全局注册一个面包屑
   }
 }
