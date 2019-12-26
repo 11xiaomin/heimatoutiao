8715f17a4a5396693872ffef757ee4a1db57b9ae
11xiaomin <2421939406@qq.com>
2019-12-27 00:08:59 +0800

    发表文章-发布文章检验逻辑实现

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -3,28 +3,28 @@
     <bread-crumb slot="header">
       <template slot="title">发布文章</template>
     </bread-crumb>
-    <el-form style="margin-left:50px" label-width="100px">
-      <el-form-item label="标题">
-        <el-input style="width:60%"></el-input>
+    <el-form ref="publishForm" :model='formData' :rules="publishRules" style="margin-left:50px" label-width="100px">
+      <el-form-item prop='title' label="标题">
+        <el-input v-model='formData.title' style="width:60%"></el-input>
       </el-form-item>
-      <el-form-item label="内容">
-        <el-input type="textarea" :rows="4"></el-input>
+      <el-form-item prop="content" label="内容">
+        <el-input v-model="formData.content" type="textarea" :rows="4"></el-input>
       </el-form-item>
-      <el-form-item label="封面">
-        <el-radio-group>
-          <el-radio>单图</el-radio>
-          <el-radio>三图</el-radio>
-          <el-radio>无图</el-radio>
-          <el-radio>自动</el-radio>
+      <el-form-item prop="type" label="封面">
+        <el-radio-group v-model="formData.cover.type">
+          <el-radio :label="1">单图</el-radio>
+          <el-radio :label="3">三图</el-radio>
+          <el-radio :label="0">无图</el-radio>
+          <el-radio :label="-1">自动</el-radio>
         </el-radio-group>
       </el-form-item>
-      <el-form-item label="频道">
-        <el-select >
+      <el-form-item prop="channel_id" label="频道">
+        <el-select v-model="formData.channel_id">
             <el-option v-for="item in channels" :key="item.id" :label="item.name" :value="item.id"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary">发布</el-button>
+        <el-button @click="publishArticle" type="primary">发布</el-button>
         <el-button>存入草稿</el-button>
       </el-form-item>
     </el-form>
@@ -35,7 +35,22 @@
 export default {
   data () {
     return {
-      channels: []
+      channels: [],
+      formData: {
+        title: '', // 标题
+        content: '', // 内容
+        cover: {// 封面
+          type: 0, // 类型
+          images: [] // 图片地址
+        },
+        channel_id: null// 频道id
+      },
+      publishRules: {
+        // 校验规则
+        title: [{ required: true, message: '标题内容不能为空' }, { min: 5, max: 30, message: '标题长度为5-30字符之间' }],
+        content: [{ required: true, message: '文章内容不能为空' }],
+        channel_id: [{ required: true, message: '频道分类不能为空' }]
+      }
     }
   },
   methods: {
@@ -44,6 +59,13 @@ export default {
         url: '/channels'
       }).then(result => {
         this.channels = result.data.channels // 获取频道数据
+      })
+    },
+    publishArticle () {
+      this.$refs.publishForm.validate(function (isOK) {
+        if (isOK) {
+
+        }
       })
     }
   },
