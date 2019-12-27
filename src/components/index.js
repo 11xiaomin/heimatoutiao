9a58fe6fc50d2d2ff1669de0e674382d6fc3ab51
11xiaomin <2421939406@qq.com>
2019-12-27 17:56:29 +0800

    发布文章-注册富文本编辑器并使用

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,10 @@
 import layoutAside from './home/layout-aside'
 import layoutHeader from './home/layout-header'
 import BreadCrumb from './comment/bread-crumb'
+import { quillEditor } from 'vue-quill-editor'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 // 所有自定义组件的插件
 export default {
   install (Vue) {
@@ -8,5 +12,6 @@ export default {
     Vue.component('layout-aside', layoutAside) // 注册 左侧导航组件
     Vue.component('layout-header', layoutHeader) // 注册头部组件
     Vue.component('bread-crumb', BreadCrumb) // 全局注册一个面包屑
+    Vue.component('quill-editor', quillEditor) // 全局注册富文本编辑器
   }
 }
